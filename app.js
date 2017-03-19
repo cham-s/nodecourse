e4c2ff86c7e52ad5a7160aa725dfedc450c64b5d
cham-s <cattouma@student.42.fr>
2017-03-19 16:03:27 +0100

    function

--- app.js
+++ app.js
@@ -2,5 +2,16 @@ var a = 1;
 var b = 2;
 var c = a + b;
 
-console.log(c);
+//console.log(c);
 
+// function statement
+function greet() {
+    console.log("Hello");
+}
+
+
+function logGr(fn) {
+    fn();
+}
+
+logGr(greet);
