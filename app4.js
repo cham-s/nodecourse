f7baedf94b9be3e2ceb2206444b8ea621ef86bc2
cham-s <cattouma@student.42.fr>
2017-03-21 18:05:44 +0100

    native

---
+++ app4.js
@@ -0,0 +1,10 @@
+var greet = require('./greet2').greet;
+var greet3 = require('./greet3');
+var Greet4 = require('./greet4');
+var greet5 = require('./greet5');
+console.log(greet3);
+greet();
+greet3.greet();
+var grtr = new Greet4();
+grtr.greet();
+greet5.greet();
