ab864056a1f7b1dfd18d47fc3fd230810e890204
cham-s <cattouma@student.42.fr>
2017-03-23 19:05:44 +0100

    server

--- stream.js
+++ stream.js
@@ -1,9 +1,11 @@
 var fs = require('fs');
+var zlib = require('zlib');
 
-var readable = fs.createReadStream(__dirname + '/lorem.txt', {encoding: 'utf8', highWaterMark: 16 * 1024});
+var readable = fs.createReadStream(__dirname + '/lorem.txt');
 var writable = fs.createWriteStream(__dirname+ '/loremcpy.txt');
+var compressed = fs.createWriteStream(__dirname+ '/lorem.txt.gz');
 
-readable.on('data', function(chunk) {
-    console.log(chunk.length);
-    writable.write(chunk);
-});
+var gzip = zlib.createGzip();
+
+readable.pipe(writable);
+readable.pipe(gzip).pipe(compressed);
